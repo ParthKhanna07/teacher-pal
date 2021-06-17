dc9cd1d5b86e6006ceb639c905d190723bf805fc
Parth Khanna <46607748+ParthKhanna07@users.noreply.github.com>
2021-06-18 03:43:28 +0530

    initial commit
    LOGIN and REGISTERATION

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Login from "./pages/login";
+import Dashboard from "./pages/dashboard";
+import Index from "./pages/index";
+
+import Register from "./pages/register";
+import NotFound from "./pages/notfound";
+
+import Home from "./pages/home";
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <Switch>
+                        <Route exact path='/' component={Login} />
+                        <Route path="/home" component={Home}></Route>
+                        <Route path='/dashboard' component={Dashboard} />
+                        <Route path='/index' component={Index}/>
+                        <Route path='/register' component={Register} />
+                        <Route path='*' component={NotFound} />
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default App;
